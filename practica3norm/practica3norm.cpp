89c2728b0c365fca94034010a2a5c52f5846bd8e
Igorymbus <igor.glazkov.96@mail.ru>
2024-02-19 12:38:10 +0300

    pepos

---
+++ practica3norm/practica3norm.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int str = 3;
+    const int stolb = 5;
+    double array[str][stolb];
+
+    cout << "Введите элементы двумерного массива (3x5):\n";
+    for (int i = 0; i < str; ++i) {
+        for (int j = 0; j < stolb; ++j) {
+            cout << "Введите элемент [" << i << "][" << j << "]: ";
+            cin >> array[i][j];
+        }
+    }
+
+    for (int i = 0; i < str; ++i) {
+        double sum = 0;
+        for (int j = 0; j < stolb; ++j) {
+            sum += array[i][j];
+        }
+        double average = sum / stolb;
+        cout << "Среднее арифметическое элементов строки " << i << ": " << average << endl;
+    }
+    return 0;
+}
+
